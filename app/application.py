81929add8b078821e9c473dd2ed8e6d3b7137a0a
Shkiperon <shkiperon@gmail.com>
2023-01-26 00:41:21 +0500

    Register task_processor blueprint in application

--- app/application.py
+++ app/application.py
@@ -1,5 +1,9 @@
 from flask import Flask
 
+from task_processor.views import task_processor_bp
+
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
+
+app.register_blueprint(task_processor_bp)
 
